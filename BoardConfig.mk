a20ebd8170eb7648ff6b5ea884153edc97786cd4
Benji Hertel <thedude@teamdouche.net>
2010-11-24 01:28:27 -0600

    Added emmc support, merged from private repo
    Change-Id: Ibc4b4841ec838b982206f64245c9c1fd75544041

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,18 @@
+#
 # Copyright (C) 2009 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 # config.mk
 #
@@ -21,10 +23,8 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
-USE_CAMERA_STUB := true
-
 # inherit from the proprietary version
--include vendor/samsung/SGH-T959/BoardConfigVendor.mk
+-include vendor/samsung/vibrant/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 #TARGET_NO_RECOVERY := true
@@ -43,26 +43,34 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 BOARD_USES_ALSA_AUDIO := true
 BUILD_WITH_ALSA_UTILS := true
 
-# WiFi related defines
+# For the FM Radio
+BOARD_HAVE_FM_RADIO := true
 
-#BOARD_WPA_SUPPLICANT_DRIVER := WEXT
-#WPA_SUPPLICANT_VERSION := VER_0_6_X
-#MFGDRV_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_mfg.bin nvram_path=/system/etc/wifi/nvram_mfg.txt"
-#DRV_AP_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_aps.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=200 dhd_poll=1"
-#BOARD_WLAN_DEVICE := bcm4329
+# WiFi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+BOARD_WLAN_DEVICE := eth0
 WIFI_DRIVER_MODULE_PATH := "/lib/modules/dhd.ko"
+MFGDRV_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_mfg.bin nvram_path=/system/etc/wifi/nvram_mfg.txt"
+DRV_AP_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_aps.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=200 dhd_poll=1"
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=10 dhd_poll=1"
 WIFI_DRIVER_MODULE_NAME := "dhd"
 
 # Bluetooth related defines
 BOARD_HAVE_BLUETOOTH_BCM := true
+
 BOARD_HAVE_BLUETOOTH := true
 BT_USE_BTL_IF := true
 BT_ALT_STACK := true
 BRCM_BTL_INCLUDE_A2DP := true
 BRCM_BT_USE_BTL_IF := true
 
-BOARD_EGL_CFG := device/samsung/vibrant/prebuilt/egl.cfg
+# Camera related defines
+USE_CAMERA_STUB := false
+BOARD_OVERLAY_FORMAT_YCbRc_420_SP := true
+BOARD_EGL_CFG := device/samsung/vibrant/prebuilt/lib/egl/egl.cfg
+BUILD_PV_VIDEO_ENCODERS := 1
+BOARD_USES_ECLAIR_CAMERA := true
 
 # Device related defines
 
@@ -96,7 +104,7 @@ BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
 BOARD_CACHE_DEVICE := /dev/block/stl11
 BOARD_CACHE_FILESYSTEM := auto
 BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_EMMC_DEVICE := /dev/block/mmcblk0p1
 BOARD_USES_BMLUTILS := true
+BOARD_HAS_SMALL_RECOVERY := true
--- full_vibrant.mk
+++ full_vibrant.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,36 +31,46 @@ PRODUCT_COPY_FILES += \
 ## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/vibrant/vibrant-vendor.mk)
 
-## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+## (3) Finally, the least specific parts, i.e. the non-GSM-specific aspects
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240 \
     rild.libpath=/system/lib/libsec-ril.so \
     rild.libargs=-d /dev/ttyS0 \
-    ro.ril.gprsclass = 10 \
-    ro.ril.hsxpa=2 \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=15
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.phone_storage=1 \
+    ro.additionalmounts=/mnt/emmc \
     dalvik.vm.startheapsize=8m \
-    dalvik.vm.heapsize=48m
+    dalvik.vm.heapsize=48m \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.dexopt-data-only=1 \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-meta=true \
+    media.stagefright.enable-scan=true \
+    media.stagefright.enable-http=true \
+    keyguard.no_require_sim=true 
 
-# Vibrant uses high-density artwork where available
+# Galaxy S uses high-density artwork where available
 PRODUCT_LOCALES := hdpi
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/vibrant/overlay
 
 # media profiles and capabilities spec
 $(call inherit-product, device/samsung/vibrant/media_a1026.mk)
+
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/samsung/vibrant/media_profiles.xml:system/etc/media_profiles.xml
-
-# asound.conf
-PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/asound.conf:system/etc/asound.conf
 
 # Install the features available on this device.
 PRODUCT_COPY_FILES += \
@@ -75,19 +85,21 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout / Keychars
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
-    device/samsung/vibrant/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
-    device/samsung/vibrant/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/samsung/vibrant/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
-    device/samsung/vibrant/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-    device/samsung/vibrant/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    device/samsung/vibrant/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
+    device/samsung/vibrant/prebuilt/usr/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
+    device/samsung/vibrant/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/vibrant/prebuilt/usr/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/vibrant/prebuilt/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin 
 
 PRODUCT_COPY_FILES += \
-    device/samsung/vibrant/prebuilt/vold.fstab:system/etc/vold.fstab 
+    device/samsung/vibrant/prebuilt/etc/asound.conf:system/etc/asound.conf \
+    device/samsung/vibrant/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
+    device/samsung/vibrant/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+
 
 # Kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -99,13 +111,11 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-
 $(call inherit-product, build/target/product/full.mk)
-
 
 PRODUCT_NAME := full_vibrant
 PRODUCT_DEVICE := vibrant
-PRODUCT_MODEL := SAMSUNG-SGH-T959
+PRODUCT_MODEL := SGH-T959
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 TARGET_IS_GALAXYS := true
