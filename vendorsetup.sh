bb54e6a90eb9ac4b25183af7e2a221622d07683c
Wes Garner <wesgarner@gmail.com>
2011-01-18 21:18:40 -0600

    Vibrant: Correct vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cyanogen_vibrant-userdebug
-add_lunch_combo cyanogen_vibrant-eng
+add_lunch_combo generic_vibrant-userdebug
+add_lunch_combo generic_vibrant-eng
